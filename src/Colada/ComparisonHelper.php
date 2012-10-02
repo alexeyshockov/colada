0946cc036e0e92252ececda27e0ee9d2ecc2ed11
Alexey Shockov <alexey@shockov.com>
2012-10-02 16:19:17 +0100

    More safe comparison

--- src/Colada/ComparisonHelper.php
+++ src/Colada/ComparisonHelper.php
@@ -18,7 +18,7 @@ class ComparisonHelper
     public static function isEquals($element1, $element2)
     {
         if (is_object($element1)) {
-            if (method_exists($element1, 'isEqualTo')) {
+            if (static::isEquationPossibleFor($element1, $element2)) {
                 return $element1->isEqualTo($element2);
             } else {
                 return ($element1 === $element2);
@@ -26,5 +26,25 @@ class ComparisonHelper
         }
 
         return ($element1 == $element2);
+    }
+
+    private static function isEquationPossibleFor($element1, $element2)
+    {
+        $class = new \ReflectionObject($element1);
+
+        $possible = false;
+        if ($class->hasMethod('isEqualTo')) {
+            $parameters = $class->getMethod('isEqualTo')->getParameters();
+            $parameter  = $parameters[0];
+
+            // NULL for non class parameters.
+            if ($parameterClass = $parameter->getClass()) {
+                if (is_object($element2) && $class->isInstance($element2)) {
+                    $possible = true;
+                }
+            }
+        }
+
+        return $possible;
     }
 }
